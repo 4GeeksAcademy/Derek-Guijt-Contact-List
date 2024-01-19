90ae49532c19457b492d4ee0ab3de3f3d237b93e
Derek Guijt <137216062+DerekGuijt@users.noreply.github.com>
2024-01-19 22:01:15 +0000

    first commit

--- src/js/views/Contacts.js
+++ src/js/views/Contacts.js
@@ -4,27 +4,44 @@ import { Context } from "../store/appContext";
 import "../../styles/Contacts.css";
 
 const Contacts = () => {
-  const { id } = useParams();
+  let params  = useParams();
   const navigate = useNavigate();
   const { store, actions } = useContext(Context);
-  const [contact, setContact] = useState({
-    name: "",
-    phone: "",
-    email: "",
-    address: "",
-  });
+  // const [contact, setContact] = useState({
+  //   name: "",
+  //   phone: "",
+  //   email: "",
+  //   address: "",
+  // });
+  const [name, setname] = useState("");
+  const [phone, setphone] = useState("");
+  const [email, setemail] = useState("");
+  const [address, setaddress] = useState("");
 
+  const loadContact = () => {
+    fetch("https://playground.4geeks.com/apis/fake/contact/" + params.id)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+      setname(data[0].full_name)
+      setaddress(data[0].address)
+      setemail(data[0].email)
+      setphone(data[0].phone)
+    });
+  }
+  const editingContact = (e) => {
+    e.preventDefault()
+    console.log(name,email,phone,address)
+    actions.editContact(name,email,phone,address)
+    setname("")
+    setaddress("")
+    setemail("")
+    setphone("")
+    navigate("/")
+  }
   useEffect(() => {
-    const foundContact = store.contacts.find((c) => c.id.toString() === id);
-    if (foundContact) {
-      setContact({
-        name: foundContact.full_name,
-        phone: foundContact.phone,
-        email: foundContact.email,
-        address: foundContact.address,
-      });
-    }
-  }, [id, store.contacts]);
+   loadContact()
+  }, []);
 
   const handleInputChange = (e) => {
     setContact({ ...contact, [e.target.name]: e.target.value });
@@ -56,36 +73,36 @@ const Contacts = () => {
           <input
             type="text"
             name="name"
-            value={contact.name}
-            onChange={handleInputChange}
+            value={name}
+            onChange={(e) => setname(e.target.value)}
           />
           <br />
           <label>Phone:</label>
           <input
             type="text"
             name="phone"
-            value={contact.phone}
-            onChange={handleInputChange}
+            value={phone}
+            onChange={(e) => setphone(e.target.value)}
           />
           <br />
           <label>Email:</label>
           <input
             type="email"
             name="email"
-            value={contact.email}
-            onChange={handleInputChange}
+            value={email}
+            onChange={(e) => setemail(e.target.value)}
           />
           <br />
           <label>Address:</label>
           <input
             type="text"
             name="address"
-            value={contact.address}
-            onChange={handleInputChange}
+            value={address}
+            onChange={(e) => setaddress(e.target.value)}
           />
           <br />
-          <button onClick={saveContact}>Save Changes</button>
-          <button onClick={deleteContact}>Delete</button>
+          <button onClick={(e) => editingContact(e)}>Save Changes</button>
+          <button onClick={() => actions.deleteContact(params.id)}>Delete</button>
         </div>
       </div>
     </div>
